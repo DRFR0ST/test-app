dbff473348208fe52a731b8c8ef8ee47c9dd5a2b
MLypchak <maksym.lypchak@hemmersbach.com>
2021-09-26 14:45:10 +0200

    add typescript files, add .eslintrc file

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  // StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App: () => React.ReactElement = () => {
+  return (
+    <View>
+      <Text>Typescript</Text>
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({});
+
+export default App;
